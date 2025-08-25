f353733e0b5c4be75685628dd63de5c3698c0a5e
WhosWho <catrixlord@gmail.com>
2025-08-25 14:00:35 +0300

    Update allvarsandfct.h

--- allvarsandfct.h
+++ allvarsandfct.h
@@ -7,8 +7,8 @@ void botdecide();//Where the bot is thinking of what to do
 int finalNum(); //Calculate the final number: the average of chosen numbers * 0.8
 extern short int player_points;
 extern short int bot_points;
-extern float player_num; //The number the player chooses
-extern float bot_num; //The number the bot chooses
+extern short int player_num; //The number the player chooses
+extern short int bot_num; //The number the bot chooses
 extern float final_num; //The final number, which is the number that gets multiplied by 0.8 after the average is calculated
 extern char c; //The choice the player will make, to either read the rules or pick a number
 extern bool aggressive; //Trigger aggressive mode for the bot
